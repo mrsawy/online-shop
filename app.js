a42ff9a5b2fc9b58727284f2842d6cf56e2f7ba7
ahmed mrsawy <102954608+mrsawy@users.noreply.github.com>
2022-12-15 14:01:52 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,84 +1,147 @@
-//--requiered imports .
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/error");
-const sequelize = require(`./util/database`);
-const Product = require(`./models/product`);
-const User = require(`./models/user`);
-const Cart = require(`./models/cart`);
-const CartItem = require(`./models/cartItem`);
-const Order = require(`./models/order`);
-const OrderItem = require(`./models/orderItem`);
-//------------------------------------------------------------------
+const path = require('path');
+require('dotenv').config()
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI = process.env.MONGO_URI
+
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+//--------------------------
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: 'sessions'
+});
+//----------------------------- configiring csrf protection
+const csrfProtection = csrf();
+//----------------------------- multer configiration
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+//-------------------------------------setting the view engine
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+
+//////////////------------using multer & bodu parser to analyse the incomming data into the requestbody & request file
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+//-----------------------------------
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+//------------------------
 
-//--attaching the userId to the request to use it b3den
+//-----------uing sessions
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+//------------
+app.use(csrfProtection); //for protection against cross site request forgeiry attacks
+app.use(flash());         // for the invalid input 
 
 app.use((req, res, next) => {
-  User.findOne({ where: { id: 1 } })
-    .then((user) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+//-------------
+
+
+
+//----- keeping the user in the request to use it later 
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(err => {
+
+      next(new Error(err));
     });
 });
 
-//------------ROUTERS-------------//
-app.use("/admin", adminRoutes);
 
+///---------setting up the routs
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
-//-----setting the relations between the models (TABLES) ;
-Product.belongsTo(User, { constraints: true, onDelete: `CASCADE` });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
 
 
-//-------------------------------------------------------------------
+//----------error handling function
 
-//{force:true}
+app.use((error, req, res, next) => {
+  console.log(error)
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
-//------------sync the models together at first --- //
-sequelize
-  .sync({force:true})
-  .then(() => {
-    return User.findOne({ where: { id: 1 } });
-  })
-  .then((user) => {
-    if (!user) {
-       User.create({ firstName: `ahmed`, email: `mrsawy@sego` }).then(u=>{
-        u.createOrder();
-        u.createCart();
-        return u;
-       });
+////--------------connecting to mongo db & setting up the server
 
-    } else {
-      return user;
-    }
-  })
-  .then(() => {
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true ,useUnifiedTopology: true})
+  .then(result => {
     app.listen(3000);
   })
-  .catch((e) => {
-    console.log(e);
+  .catch(err => {
+    console.log(err);
   });
